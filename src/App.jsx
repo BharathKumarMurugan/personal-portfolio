c08aaa6c878d158c6515f9cf32f76359355f13e4
bharathkumarmurugan2016 <bharathkumarmurugan2016@gmail.com>
2024-08-31 15:11:36 +0000

    created about comp

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import "boxicons";
 import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import About from "./components/about/About";
 
 function App() {
 
   return (
     <>
       {/* https://www.youtube.com/watch?v=3aCoZudPEKE&list=PLd0qRSEDFQI7DgB5BKHQrryFlR02n1bK-&index=5 */}
-      {/* start at 51:56 */}
+      {/* start at 1:02:07 */}
       <Header />
       
       <main className="main">
         <Home />
+        <About />
       </main>
     </>
   )
---
+++ src/components/about/About.jsx
@@ -0,0 +1,19 @@
+import "./about.css";
+
+function About() {
+  return (
+    <section className="about section" id="about">
+        <h2 className="section__title">
+            About Me
+        </h2>
+        <span className="section__subtit">
+            My Introduction
+        </span>
+        <div className="container about__container grid">
+            
+        </div>
+    </section>
+  )
+}
+
+export default About
